1d0b2b0ba6888a1aa19b1cc348f808fa5ebb307b
gabrieltoledoo <rm551654@fiap.com.br>
2023-09-17 13:18:12 -0300

    atualizando nav

---
+++ src/componentes/Error.jsx
@@ -0,0 +1,14 @@
+import {  } from 'react'
+
+
+function Error() {
+
+{/* componente error quando tentar entrar em uma componente(página) errado*/}
+  return (
+    <>
+      <h1>404-Página não encontrada</h1>
+    </>
+  )
+}
+
+export default Error
---
+++ src/componentes/Nav.jsx
@@ -0,0 +1,18 @@
+import {} from 'react';
+import {Link} from 'react-router-dom'
+import styles from '../css/Nav.module.css';
+
+function Nav() {
+   {/* componente do nav e importando o css module usando link */}
+  return (
+    <section className={styles.nav}>
+      <Link to="home" className={tlink}>Home</Link>
+      <Link to="contato" className={tlink}>Contato</Link>
+      <Link to="calculadora" className={tlink}>Calculadora</Link>
+      <Link to="sobrenos" className={tlink}>sobreNos</Link>
+      <Link to="error" className={tlink}>Error</Link>
+    </section>
+  );
+}
+
+export default Nav;
